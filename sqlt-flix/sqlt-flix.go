c8c52181f839376938a8f5b688259ad324cce98d
Malte Wrogemann <wroge.malte@gmail.com>
2025-04-03 21:39:54 +0200

    update charts

--- sqlt-flix/sqlt-flix.go
+++ sqlt-flix/sqlt-flix.go
@@ -9,6 +9,255 @@ import (
 	"github.com/wroge/sqlt"
 )
 
+var (
+	schema = sqlt.Exec[any](
+		sqlt.Parse(`
+			CREATE TABLE movies (
+				id INTEGER PRIMARY KEY,
+				title TEXT NOT NULL,
+				added_at DATE NOT NULL,
+				rating NUMERIC NOT NULL
+			);
+
+			CREATE TABLE people (
+				id INTEGER PRIMARY KEY,
+				name TEXT NOT NULL UNIQUE
+			);
+
+			CREATE TABLE movie_directors (
+				movie_id INTEGER REFERENCES movies (id),
+				person_id INTEGER REFERENCES people (id),
+				PRIMARY KEY (movie_id, person_id)
+			);
+
+			CREATE TABLE movie_actors (
+				movie_id INTEGER REFERENCES movies (id),
+				person_id INTEGER REFERENCES people (id),
+				PRIMARY KEY (movie_id, person_id)
+			);
+
+			CREATE TABLE countries (
+				id INTEGER PRIMARY KEY,
+				name TEXT NOT NULL UNIQUE
+			);
+
+			CREATE TABLE movie_countries (
+				movie_id INTEGER REFERENCES movies (id),
+				country_id INTEGER REFERENCES countries (id),
+				PRIMARY KEY (movie_id, country_id)
+			);
+
+			CREATE TABLE genres (
+				id INTEGER PRIMARY KEY,
+				name TEXT NOT NULL UNIQUE
+			);
+
+			CREATE TABLE movie_genres (
+				movie_id INTEGER REFERENCES movies (id),
+				genre_id INTEGER REFERENCES genres (id),
+				PRIMARY KEY (movie_id, genre_id)
+			);
+		`),
+	)
+
+	create = sqlt.Transaction(nil,
+		sqlt.Exec[benchflix.Movie](sqlt.Parse(`
+			INSERT INTO movies (id, title, added_at, rating) VALUES 
+			({{ .ID }}, {{ .Title }}, {{ .AddedAt }}, {{ .Rating }});
+		`)),
+		sqlt.All[benchflix.Movie, int64](sqlt.Name("DirectorIDs"), sqlt.Parse(`
+			{{ if .Directors }}
+				INSERT INTO people (name) VALUES 
+				{{ range $i, $p := .Directors }}
+					{{ if $i }}, {{ end }}
+					({{ $p }})
+				{{ end }}
+				ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
+			{{ end }}
+		`)),
+		sqlt.All[benchflix.Movie, int64](sqlt.Name("ActorIDs"), sqlt.Parse(`
+			{{ if .Actors }}
+				INSERT INTO people (name) VALUES 
+				{{ range $i, $p := .Actors }}
+					{{ if $i }}, {{ end }}
+					({{ $p }})
+				{{ end }}
+				ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
+			{{ end }}
+		`)),
+		sqlt.Exec[benchflix.Movie](sqlt.Parse(`
+			{{ if .Directors }}
+				INSERT INTO movie_directors (movie_id, person_id) VALUES
+				{{ range $i, $id := (Context "DirectorIDs") }}
+					{{ if $i }}, {{ end }}
+					({{ $.ID }}, {{ $id }})
+				{{ end }}
+			{{ end }}
+		`)),
+		sqlt.Exec[benchflix.Movie](sqlt.Parse(`
+			{{ if .Actors }}
+				INSERT INTO movie_actors (movie_id, person_id) VALUES
+				{{ range $i, $id := (Context "ActorIDs") }}
+					{{ if $i }}, {{ end }}
+					({{ $.ID }}, {{ $id }})
+				{{ end }}
+			{{ end }}
+		`)),
+		sqlt.All[benchflix.Movie, int64](sqlt.Name("CountryIDs"), sqlt.Parse(`
+			{{ if .Countries }}
+				INSERT INTO countries (name) VALUES 
+				{{ range $i, $p := .Countries }}
+					{{ if $i }}, {{ end }}
+					({{ $p }})
+				{{ end }}
+				ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
+			{{ end }}
+		`)),
+		sqlt.Exec[benchflix.Movie](sqlt.Parse(`
+			{{ if .Countries }}
+				INSERT INTO movie_countries (movie_id, country_id) VALUES
+				{{ range $i, $id := (Context "CountryIDs") }}
+					{{ if $i }}, {{ end }}
+					({{ $.ID }}, {{ $id }})
+				{{ end }}
+			{{ end }}
+		`)),
+		sqlt.All[benchflix.Movie, int64](sqlt.Name("GenreIDs"), sqlt.Parse(`
+			{{ if .Genres }}
+				INSERT INTO genres (name) VALUES 
+				{{ range $i, $p := .Genres }}
+					{{ if $i }}, {{ end }}
+					({{ $p }})
+				{{ end }}
+				ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
+			{{ end }}
+		`)),
+		sqlt.Exec[benchflix.Movie](sqlt.Parse(`
+			{{ if .Genres }}
+				INSERT INTO movie_genres (movie_id, genre_id) VALUES
+				{{ range $i, $id := (Context "GenreIDs") }}
+					{{ if $i }}, {{ end }}
+					({{ $.ID }}, {{ $id }})
+				{{ end }}
+			{{ end }}
+		`)),
+	)
+
+	first = sqlt.First[int64, benchflix.Movie](sqlt.Cache{}, sqlt.Parse(`
+		SELECT
+			movies.id,		{{ ScanInt64 "ID" }}
+			movies.title,		{{ ScanString "Title" }}
+			movies.added_at,	{{ ScanTime "AddedAt" }}
+			movies.rating,		{{ ScanFloat64 "Rating" }}
+			(
+				SELECT GROUP_CONCAT(people.name ORDER BY people.name)
+				FROM movie_directors
+				JOIN people ON people.id = movie_directors.person_id
+				WHERE movie_directors.movie_id = movies.id
+			) AS directors,		{{ ScanStringSlice "Directors" "," }}
+			(
+				SELECT GROUP_CONCAT(people.name ORDER BY people.name)
+				FROM movie_actors
+				JOIN people ON people.id = movie_actors.person_id
+				WHERE movie_actors.movie_id = movies.id
+			) AS actors,		{{ ScanStringSlice "Actors" "," }}
+			(
+				SELECT GROUP_CONCAT(countries.name ORDER BY countries.name)
+				FROM movie_countries
+				JOIN countries ON countries.id = movie_countries.country_id
+				WHERE movie_countries.movie_id = movies.id
+			) AS countries,		{{ ScanStringSlice "Countries" "," }}
+			(
+				SELECT GROUP_CONCAT(genres.name ORDER BY genres.name)
+				FROM movie_genres
+				JOIN genres ON genres.id = movie_genres.genre_id
+				WHERE movie_genres.movie_id = movies.id
+			) AS genres 		{{ ScanStringSlice "Genres" "," }}
+		FROM movies
+		WHERE movies.id = {{ . }}
+		ORDER BY movies.title ASC;
+	`))
+
+	all = sqlt.All[benchflix.Query, benchflix.Movie](sqlt.Cache{}, sqlt.Parse(`
+		SELECT
+			movies.id,		{{ ScanInt64 "ID" }}
+			movies.title,		{{ ScanString "Title" }}
+			movies.added_at,	{{ ScanTime "AddedAt" }}
+			movies.rating,		{{ ScanFloat64 "Rating" }}
+			(
+				SELECT GROUP_CONCAT(people.name ORDER BY people.name)
+				FROM movie_directors
+				JOIN people ON people.id = movie_directors.person_id
+				WHERE movie_directors.movie_id = movies.id
+			) AS directors,		{{ ScanStringSlice "Directors" "," }}
+			(
+				SELECT GROUP_CONCAT(people.name ORDER BY people.name)
+				FROM movie_actors
+				JOIN people ON people.id = movie_actors.person_id
+				WHERE movie_actors.movie_id = movies.id
+			) AS actors,		{{ ScanStringSlice "Actors" "," }}
+			(
+				SELECT GROUP_CONCAT(countries.name ORDER BY countries.name)
+				FROM movie_countries
+				JOIN countries ON countries.id = movie_countries.country_id
+				WHERE movie_countries.movie_id = movies.id
+			) AS countries,		{{ ScanStringSlice "Countries" "," }}
+			(
+				SELECT GROUP_CONCAT(genres.name ORDER BY genres.name)
+				FROM movie_genres
+				JOIN genres ON genres.id = movie_genres.genre_id
+				WHERE movie_genres.movie_id = movies.id
+			) AS genres 		{{ ScanStringSlice "Genres" "," }}
+		FROM movies
+		WHERE 1=1
+		{{ if .Search }}
+			AND (
+				EXISTS (
+					SELECT 1 FROM movie_directors
+					JOIN people ON people.id = movie_directors.person_id
+					WHERE movie_directors.movie_id = movies.id
+					AND INSTR(people.name, {{ .Search }}) > 0
+				)
+				OR EXISTS (
+					SELECT 1 FROM movie_actors
+					JOIN people ON people.id = movie_actors.person_id
+					WHERE movie_actors.movie_id = movies.id
+					AND INSTR(people.name, {{ .Search }}) > 0
+				)
+			)
+		{{ end }}
+		{{ if .Genre }}
+			AND EXISTS (
+				SELECT 1 FROM movie_genres
+				JOIN genres ON genres.id = movie_genres.genre_id
+				WHERE movie_genres.movie_id = movies.id
+				AND genres.name = {{ .Genre }}
+			)
+		{{ end }}
+		{{ if .Country }}
+			AND EXISTS (
+				SELECT 1 FROM movie_countries
+				JOIN countries ON countries.id = movie_countries.country_id
+				WHERE movie_countries.movie_id = movies.id
+				AND countries.name = {{ .Country }}
+			)
+		{{ end }}
+		{{ if not .AddedBefore.IsZero }}
+			AND added_at < {{ .AddedBefore }}
+		{{ end }}
+		{{ if not .AddedAfter.IsZero }}
+			AND added_at > {{ .AddedAfter }}
+		{{ end }}
+		{{ if .MinRating }}
+			AND rating >= {{ .MinRating }}
+		{{ end }}
+		{{ if .MaxRating }}
+			AND rating <= {{ .MaxRating }}
+		{{ end }}
+		ORDER BY movies.title ASC;
+	`))
+)
+
 func NewRepository() benchflix.Repository {
 	db, err := sql.Open("sqlite3", ":memory:?_fk=1")
 	if err != nil {
@@ -17,252 +266,9 @@ func NewRepository() benchflix.Repository {
 
 	r := Repository{
 		DB: db,
-		SchemaSqlt: sqlt.Exec[any](
-			sqlt.Parse(`
-				CREATE TABLE movies (
-					id INTEGER PRIMARY KEY,
-					title TEXT NOT NULL,
-					added_at DATE NOT NULL,
-					rating NUMERIC NOT NULL
-				);
-	
-				CREATE TABLE people (
-					id INTEGER PRIMARY KEY,
-					name TEXT NOT NULL UNIQUE
-				);
-	
-				CREATE TABLE movie_directors (
-					movie_id INTEGER REFERENCES movies (id),
-					person_id INTEGER REFERENCES people (id),
-					PRIMARY KEY (movie_id, person_id)
-				);
-	
-				CREATE TABLE movie_actors (
-					movie_id INTEGER REFERENCES movies (id),
-					person_id INTEGER REFERENCES people (id),
-					PRIMARY KEY (movie_id, person_id)
-				);
-	
-				CREATE TABLE countries (
-					id INTEGER PRIMARY KEY,
-					name TEXT NOT NULL UNIQUE
-				);
-	
-				CREATE TABLE movie_countries (
-					movie_id INTEGER REFERENCES movies (id),
-					country_id INTEGER REFERENCES countries (id),
-					PRIMARY KEY (movie_id, country_id)
-				);
-	
-				CREATE TABLE genres (
-					id INTEGER PRIMARY KEY,
-					name TEXT NOT NULL UNIQUE
-				);
-	
-				CREATE TABLE movie_genres (
-					movie_id INTEGER REFERENCES movies (id),
-					genre_id INTEGER REFERENCES genres (id),
-					PRIMARY KEY (movie_id, genre_id)
-				);
-			`),
-		),
-		CreateSqlt: sqlt.Transaction(nil,
-			sqlt.Exec[benchflix.Movie](sqlt.Parse(`
-				INSERT INTO movies (id, title, added_at, rating) VALUES 
-				({{ .ID }}, {{ .Title }}, {{ .AddedAt }}, {{ .Rating }});
-			`)),
-			sqlt.All[benchflix.Movie, int64](sqlt.Name("DirectorIDs"), sqlt.Parse(`
-				{{ if .Directors }}
-					INSERT INTO people (name) VALUES 
-					{{ range $i, $p := .Directors }}
-						{{ if $i }}, {{ end }}
-						({{ $p }})
-					{{ end }}
-					ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
-				{{ end }}
-			`)),
-			sqlt.All[benchflix.Movie, int64](sqlt.Name("ActorIDs"), sqlt.Parse(`
-				{{ if .Actors }}
-					INSERT INTO people (name) VALUES 
-					{{ range $i, $p := .Actors }}
-						{{ if $i }}, {{ end }}
-						({{ $p }})
-					{{ end }}
-					ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
-				{{ end }}
-			`)),
-			sqlt.Exec[benchflix.Movie](sqlt.Parse(`
-				{{ if .Directors }}
-					INSERT INTO movie_directors (movie_id, person_id) VALUES
-					{{ range $i, $id := (Context "DirectorIDs") }}
-						{{ if $i }}, {{ end }}
-						({{ $.ID }}, {{ $id }})
-					{{ end }}
-				{{ end }}
-			`)),
-			sqlt.Exec[benchflix.Movie](sqlt.Parse(`
-				{{ if .Actors }}
-					INSERT INTO movie_actors (movie_id, person_id) VALUES
-					{{ range $i, $id := (Context "ActorIDs") }}
-						{{ if $i }}, {{ end }}
-						({{ $.ID }}, {{ $id }})
-					{{ end }}
-				{{ end }}
-			`)),
-			sqlt.All[benchflix.Movie, int64](sqlt.Name("CountryIDs"), sqlt.Parse(`
-				{{ if .Countries }}
-					INSERT INTO countries (name) VALUES 
-					{{ range $i, $p := .Countries }}
-						{{ if $i }}, {{ end }}
-						({{ $p }})
-					{{ end }}
-					ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
-				{{ end }}
-			`)),
-			sqlt.Exec[benchflix.Movie](sqlt.Parse(`
-				{{ if .Countries }}
-					INSERT INTO movie_countries (movie_id, country_id) VALUES
-					{{ range $i, $id := (Context "CountryIDs") }}
-						{{ if $i }}, {{ end }}
-						({{ $.ID }}, {{ $id }})
-					{{ end }}
-				{{ end }}
-			`)),
-			sqlt.All[benchflix.Movie, int64](sqlt.Name("GenreIDs"), sqlt.Parse(`
-				{{ if .Genres }}
-					INSERT INTO genres (name) VALUES 
-					{{ range $i, $p := .Genres }}
-						{{ if $i }}, {{ end }}
-						({{ $p }})
-					{{ end }}
-					ON CONFLICT (name) DO UPDATE SET name = EXCLUDED.name RETURNING id;
-				{{ end }}
-			`)),
-			sqlt.Exec[benchflix.Movie](sqlt.Parse(`
-				{{ if .Genres }}
-					INSERT INTO movie_genres (movie_id, genre_id) VALUES
-					{{ range $i, $id := (Context "GenreIDs") }}
-						{{ if $i }}, {{ end }}
-						({{ $.ID }}, {{ $id }})
-					{{ end }}
-				{{ end }}
-			`)),
-		),
-		OneSqlt: sqlt.First[int64, benchflix.Movie](sqlt.Cache{}, sqlt.Parse(`
-			SELECT
-				movies.id,		{{ ScanInt64 "ID" }}
-				movies.title,		{{ ScanString "Title" }}
-				movies.added_at,	{{ ScanTime "AddedAt" }}
-				movies.rating,		{{ ScanFloat64 "Rating" }}
-				(
-					SELECT GROUP_CONCAT(people.name ORDER BY people.name)
-					FROM movie_directors
-					JOIN people ON people.id = movie_directors.person_id
-					WHERE movie_directors.movie_id = movies.id
-				) AS directors,		{{ ScanStringSlice "Directors" "," }}
-				(
-					SELECT GROUP_CONCAT(people.name ORDER BY people.name)
-					FROM movie_actors
-					JOIN people ON people.id = movie_actors.person_id
-					WHERE movie_actors.movie_id = movies.id
-				) AS actors,		{{ ScanStringSlice "Actors" "," }}
-				(
-					SELECT GROUP_CONCAT(countries.name ORDER BY countries.name)
-					FROM movie_countries
-					JOIN countries ON countries.id = movie_countries.country_id
-					WHERE movie_countries.movie_id = movies.id
-				) AS countries,		{{ ScanStringSlice "Countries" "," }}
-				(
-					SELECT GROUP_CONCAT(genres.name ORDER BY genres.name)
-					FROM movie_genres
-					JOIN genres ON genres.id = movie_genres.genre_id
-					WHERE movie_genres.movie_id = movies.id
-				) AS genres 		{{ ScanStringSlice "Genres" "," }}
-			FROM movies
-			WHERE movies.id = {{ . }}
-			ORDER BY movies.title ASC;
-		`)),
-		AllSqlt: sqlt.All[benchflix.Query, benchflix.Movie](sqlt.Cache{}, sqlt.Parse(`
-			SELECT
-				movies.id,		{{ ScanInt64 "ID" }}
-				movies.title,		{{ ScanString "Title" }}
-				movies.added_at,	{{ ScanTime "AddedAt" }}
-				movies.rating,		{{ ScanFloat64 "Rating" }}
-				(
-					SELECT GROUP_CONCAT(people.name ORDER BY people.name)
-					FROM movie_directors
-					JOIN people ON people.id = movie_directors.person_id
-					WHERE movie_directors.movie_id = movies.id
-				) AS directors,		{{ ScanStringSlice "Directors" "," }}
-				(
-					SELECT GROUP_CONCAT(people.name ORDER BY people.name)
-					FROM movie_actors
-					JOIN people ON people.id = movie_actors.person_id
-					WHERE movie_actors.movie_id = movies.id
-				) AS actors,		{{ ScanStringSlice "Actors" "," }}
-				(
-					SELECT GROUP_CONCAT(countries.name ORDER BY countries.name)
-					FROM movie_countries
-					JOIN countries ON countries.id = movie_countries.country_id
-					WHERE movie_countries.movie_id = movies.id
-				) AS countries,		{{ ScanStringSlice "Countries" "," }}
-				(
-					SELECT GROUP_CONCAT(genres.name ORDER BY genres.name)
-					FROM movie_genres
-					JOIN genres ON genres.id = movie_genres.genre_id
-					WHERE movie_genres.movie_id = movies.id
-				) AS genres 		{{ ScanStringSlice "Genres" "," }}
-			FROM movies
-			WHERE 1=1
-			{{ if .Search }}
-				AND (
-					EXISTS (
-						SELECT 1 FROM movie_directors
-						JOIN people ON people.id = movie_directors.person_id
-						WHERE movie_directors.movie_id = movies.id
-						AND INSTR(people.name, {{ .Search }}) > 0
-					)
-					OR EXISTS (
-						SELECT 1 FROM movie_actors
-						JOIN people ON people.id = movie_actors.person_id
-						WHERE movie_actors.movie_id = movies.id
-						AND INSTR(people.name, {{ .Search }}) > 0
-					)
-				)
-			{{ end }}
-			{{ if .Genre }}
-				AND EXISTS (
-					SELECT 1 FROM movie_genres
-					JOIN genres ON genres.id = movie_genres.genre_id
-					WHERE movie_genres.movie_id = movies.id
-					AND genres.name = {{ .Genre }}
-				)
-			{{ end }}
-			{{ if .Country }}
-				AND EXISTS (
-					SELECT 1 FROM movie_countries
-					JOIN countries ON countries.id = movie_countries.country_id
-					WHERE movie_countries.movie_id = movies.id
-					AND countries.name = {{ .Country }}
-				)
-			{{ end }}
-			{{ if not .AddedBefore.IsZero }}
-				AND added_at < {{ .AddedBefore }}
-			{{ end }}
-			{{ if not .AddedAfter.IsZero }}
-				AND added_at > {{ .AddedAfter }}
-			{{ end }}
-			{{ if .MinRating }}
-				AND rating >= {{ .MinRating }}
-			{{ end }}
-			{{ if .MaxRating }}
-				AND rating <= {{ .MaxRating }}
-			{{ end }}
-			ORDER BY movies.title ASC;
-		`)),
 	}
 
-	if _, err = r.SchemaSqlt.Exec(context.Background(), db, nil); err != nil {
+	if _, err = schema.Exec(context.Background(), db, nil); err != nil {
 		panic(err)
 	}
 
@@ -270,26 +276,22 @@ func NewRepository() benchflix.Repository {
 }
 
 type Repository struct {
-	DB         *sql.DB
-	SchemaSqlt *sqlt.Statement[any, any, sql.Result]
-	CreateSqlt *sqlt.TransactionStatement[benchflix.Movie]
-	OneSqlt    *sqlt.Statement[int64, benchflix.Movie, benchflix.Movie]
-	AllSqlt    *sqlt.Statement[benchflix.Query, benchflix.Movie, []benchflix.Movie]
+	DB *sql.DB
 }
 
 // Create implements benchflix.Repository.
 func (r Repository) Create(ctx context.Context, movie benchflix.Movie) error {
-	_, err := r.CreateSqlt.Exec(ctx, r.DB, movie)
+	_, err := create.Exec(ctx, r.DB, movie)
 
 	return err
 }
 
 // Query implements benchflix.Repository.
 func (r Repository) Query(ctx context.Context, query benchflix.Query) ([]benchflix.Movie, error) {
-	return r.AllSqlt.Exec(ctx, r.DB, query)
+	return all.Exec(ctx, r.DB, query)
 }
 
 // Read implements benchflix.Repository.
 func (r Repository) Read(ctx context.Context, id int64) (benchflix.Movie, error) {
-	return r.OneSqlt.Exec(ctx, r.DB, id)
+	return first.Exec(ctx, r.DB, id)
 }
